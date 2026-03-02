c523d451ed2b306890313b33c63cde2f9ac8564e
Scott Gerstemeier <gerstemeier02@gmail.com>
2026-03-01 18:51:56 -0600

    more robust logging

--- orchestration/__init__.py
+++ orchestration/__init__.py
@@ -1,8 +1,15 @@
+# orchestration/__init__.py
 import logging
 import azure.functions as func
+import traceback
 from main import main as run_script
 
 def main(mytimer: func.TimerRequest) -> None:
-    logging.info("Weather job started.")
-    run_script()
-    logging.info("Weather job finished.")
+    logging.info("Timer triggered")
+    try:
+        run_script()
+    except Exception as e:
+        logging.error("ERROR OCCURRED:")
+        logging.error(str(e))
+        logging.error(traceback.format_exc())
+        raise
